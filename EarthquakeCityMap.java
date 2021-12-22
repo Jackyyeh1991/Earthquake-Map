2338d8b1f5a1d14ce2ff9f28bfdc14e4b84158db
Jack Yeh <jackyyeh1991@gmail.com>
2021-12-22 13:00:20 -0600

    add feature

--- EarthquakeCityMap.java
+++ EarthquakeCityMap.java
@@ -2,6 +2,8 @@ package guimodule;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
@@ -26,6 +28,7 @@ public class EarthquakeCityMap extends PApplet{
 		
 		
 		
+		
 		size(400,400);
 //		size(400,400,P2D);                                                            // P2D need OPenGL, but solve OPENGL issue latter
 		map = new UnfoldingMap(this,200,50,700,500);
@@ -34,9 +37,15 @@ public class EarthquakeCityMap extends PApplet{
 		MapUtils.createDefaultEventDispatcher(this, map);                             // interactive map
 		
 //		------------------------
-//		Add marker
+//		Add marker and Feature class
 //		------------------------
 		Location valLoc = new Location(29.71f,-95.41f);
+			
+		
+		Feature valEq = new PointFeature(valLoc);
+		
+		
+		
 		Marker val = new SimplePointMarker(valLoc);
 		map.addMarker(val);                                							 // At the end, associate with the map
 			
