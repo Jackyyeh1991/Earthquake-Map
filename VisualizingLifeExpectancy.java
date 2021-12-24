8195aecce71fa0c09807167540e023ba425d13fa
Jack Yeh <jackyyeh1991@gmail.com>
2021-12-24 14:52:40 -0600

    finish life

--- VisualizingLifeExpectancy.java
+++ VisualizingLifeExpectancy.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.Marker;
@@ -21,18 +22,36 @@ import processing.core.*;
 public class VisualizingLifeExpectancy extends PApplet{
 	
 	
-	private UnfoldingMap map;
+	UnfoldingMap map;
+	
+	List<Feature> countries;
+	List<Marker> countryMarkers;
+	Map<String, Float> lifeExpByCountry;
 	
 	public void setup (){
-		size(500,500);
+		size(860,700);
 		
 		map = new UnfoldingMap(this, 50,50,700,500);
 		map.zoomToLevel(2);
 		
 		// import file and read
-		Map<String, Float> lifeExpByCountry;
-//		lifeExpByCountry = LoadLifeExpectancyFromCSV("/Users/jacky/eclipse-workspace/UCSDUnfoldingMaps/data/LifeExpectancyWorldBank.csv");
+//		Map<String, Float> lifeExpByCountry;
 		lifeExpByCountry = LoadLifeExpectancyFromCSV("/Users/jacky/eclipse-workspace/UCSDUnfoldingMaps/data/LifeExpectancyWorldBank.csv");
+
+
+		
+//		----------------------
+//		feature list 
+//		----------------------
+		List<Feature> countries = new ArrayList<Feature>();
+		
+		
+		countries = GeoJSONReader.loadData(this, "/Users/jacky/eclipse-workspace/UCSDUnfoldingMaps/data/countries.geo.json");
+		countryMarkers = MapUtils.createSimpleMarkers(countries);
+		
+		map.addMarkers(countryMarkers);
+		shadeCountries();
+		
 		
 		MapUtils.createDefaultEventDispatcher(this, map);
 	}
@@ -54,14 +73,8 @@ public class VisualizingLifeExpectancy extends PApplet{
 			String[] columns = row.split(",");
 
 //			System.out.println(Arrays.toString(columns));
-			
-		
-
 //			float value = Float.parseFloat(columns[5]);
-			
-//
 //			System.out.println(columns[5]);
-////			
 //			System.out.println(columns[4]);
 			
 						
@@ -76,5 +89,32 @@ public class VisualizingLifeExpectancy extends PApplet{
 		return lifeExpMap;
 		
 	}
+	
+	
+	private void shadeCountries() {                              // change the number into according color, take maker, find marker countries' age using ID and set marker color.
+		for(Marker marker: countryMarkers) {
+			String countryId = marker.getId();
+			
+			if(lifeExpByCountry.containsKey(countryId)) {
+				float lifeExp = lifeExpByCountry.get(countryId);
+				int colorLevel = (int)map(lifeExp,40,90,10,255); // age: 40~90. color range: 10~255
+				marker.setColor(color(255-colorLevel,100,colorLevel));
+			}
+			else {
+				marker.setColor(color(150,150,150));
+				
+			}
+			
+		}
+		
+		 
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
