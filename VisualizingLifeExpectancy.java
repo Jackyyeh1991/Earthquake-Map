e8b5d6f09ff77fa5bd73224b952aaebecec33343
Jack Yeh <jackyyeh1991@gmail.com>
2021-12-23 22:20:08 -0600

    modify

--- VisualizingLifeExpectancy.java
+++ VisualizingLifeExpectancy.java
@@ -26,7 +26,12 @@ public class VisualizingLifeExpectancy extends PApplet{
 		size(500,500);
 		
 		map = new UnfoldingMap(this, 50,50,700,500);
-		map.zoomToLevel(2);	
+		map.zoomToLevel(2);
+		
+		// import file and read
+		Map<String, Float> lifeExpByCountry;
+		lifeExpByCountry = LoadLifeExpectancyFromCSV("/Users/jacky/eclipse-workspace/UCSDUnfoldingMaps/data/LifeExpectancyWorldBank.csv");
+		
 		MapUtils.createDefaultEventDispatcher(this, map);
 	}
 	
@@ -37,10 +42,21 @@ public class VisualizingLifeExpectancy extends PApplet{
 	}
 	
 	
-	private Map<String,Float> LoadLifeExpectancyFromCSV(String fileName){
+	private Map<String,Float> LoadLifeExpectancyFromCSV(String fileName){      // helper function to read file and store as hashmap
 		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
-		String[] rows = loadStrings(fileName);
-		System.out.println(rows);
+		String[] rows = loadStrings(fileName);               // take one row at a time and put in rows(from processing)
+		
+		for(String row : rows) {
+			String[] columns = row.split(",");
+//			if() {
+//				
+//			}
+			float value = Float.parseFloat(columns[5]);
+			lifeExpMap.put(columns[4], value);
+			
+			
+		}
+		
 		
 		return lifeExpMap;
 		
