a02050f50014621976202292c55fb467c5c32557
Jack Yeh <jackyyeh1991@gmail.com>
2021-12-23 20:47:14 -0600

    read data

--- VisualizingLifeExpectancy.java
+++ VisualizingLifeExpectancy.java
@@ -1,21 +1,48 @@
 package guimodule;
 
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import de.fhpotsdam.unfolding.UnfoldingMap;
-import processing.core.PApplet;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import processing.core.*;
 
 public class VisualizingLifeExpectancy extends PApplet{
 	
 	
-	 UnfoldingMap map;
+	private UnfoldingMap map;
 	
-	
-	public void step (){
-		size(50,50);
+	public void setup (){
+		size(500,500);
 		
-		
+		map = new UnfoldingMap(this, 50,50,700,500);
+		map.zoomToLevel(2);	
+		MapUtils.createDefaultEventDispatcher(this, map);
 	}
 	
 	public void draw(){
+		background(10);
+		map.draw();
+		
+	}
+	
+	
+	private Map<String,Float> LoadLifeExpectancyFromCSV(String fileName){
+		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
+		String[] rows = loadStrings(fileName);
+		System.out.println(rows);
+		
+		return lifeExpMap;
 		
 	}
 
