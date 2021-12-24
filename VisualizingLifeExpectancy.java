325f2fa54a405815077234d5a7b5bbcc9fd774f4
Jack Yeh <jackyyeh1991@gmail.com>
2021-12-24 13:52:13 -0600

    process CSV file

--- VisualizingLifeExpectancy.java
+++ VisualizingLifeExpectancy.java
@@ -5,6 +5,7 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +31,7 @@ public class VisualizingLifeExpectancy extends PApplet{
 		
 		// import file and read
 		Map<String, Float> lifeExpByCountry;
+//		lifeExpByCountry = LoadLifeExpectancyFromCSV("/Users/jacky/eclipse-workspace/UCSDUnfoldingMaps/data/LifeExpectancyWorldBank.csv");
 		lifeExpByCountry = LoadLifeExpectancyFromCSV("/Users/jacky/eclipse-workspace/UCSDUnfoldingMaps/data/LifeExpectancyWorldBank.csv");
 		
 		MapUtils.createDefaultEventDispatcher(this, map);
@@ -44,19 +46,32 @@ public class VisualizingLifeExpectancy extends PApplet{
 	
 	private Map<String,Float> LoadLifeExpectancyFromCSV(String fileName){      // helper function to read file and store as hashmap
 		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
-		String[] rows = loadStrings(fileName);               // take one row at a time and put in rows(from processing)
+		String[] rows = loadStrings(fileName);               // take one row at a time and put in rows(from processing) 
 		
+//		System.out.println(Arrays.toString(rows));           // show in console
+
 		for(String row : rows) {
 			String[] columns = row.split(",");
-//			if() {
-//				
-//			}
-			float value = Float.parseFloat(columns[5]);
-			lifeExpMap.put(columns[4], value);
+
+//			System.out.println(Arrays.toString(columns));
 			
+		
+
+//			float value = Float.parseFloat(columns[5]);
+			
+//
+//			System.out.println(columns[5]);
+////			
+//			System.out.println(columns[4]);
+			
+						
+			
+			if(! columns[5].equals("..")) {                      // pre-process the CSV file 
+				float value = Float.parseFloat(columns[5]);
+				lifeExpMap.put(columns[4], value);	
+			}
 			
 		}
-		
 		
 		return lifeExpMap;
 		
